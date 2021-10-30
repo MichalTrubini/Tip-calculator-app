53b666f5b8f882d4904871354a839f7aeb0b1f3e
Michal Trubini <mchl.trubini@gmail.com>
2021-10-30 19:12:00 +0200

    finished js

---
+++ js/script.js
@@ -0,0 +1,83 @@
+//GLOBAL VARIABLES
+
+const inputBillElement = document.getElementById('input-bill');
+const tipElement = document.querySelectorAll('.tip-percent');
+const tipPercentCustomElement = document.getElementById('tip-percent-custom');
+const numberOfPeopleElement = document.getElementById('number-of-people');
+const tipPerPersonElement = document.getElementById('tip-per-person');
+const totalPerPersonElement = document.getElementById('total-per-person');
+const clearStorage = document.getElementById('clear-storage');
+
+//CODE...
+
+tipElement.forEach(getTipPercent);
+inputBillElement.addEventListener('input',billAmount);
+tipPercentCustomElement.addEventListener('input',customPercentage)
+numberOfPeopleElement.addEventListener('input',numberPeople);
+clearStorage.addEventListener('click',clearLocalStorage);
+
+//FUNCTIONS
+
+function billAmount () {
+    const billAmount = inputBillElement.value;
+    localStorage.setItem('bill',billAmount);
+    calculateTip();
+}
+
+function customPercentage() {
+    const customPercent = tipPercentCustomElement.value / 100;
+    localStorage.setItem('tip',customPercent);
+    calculateTip();
+}
+
+function getTipPercent(tip){
+    tip.addEventListener('click',function(){
+        const tipNumber = parseInt(tip.textContent.replace('%','')) / 100;
+        localStorage.setItem('tip',tipNumber);
+        calculateTip();
+    })  
+}
+
+function numberPeople () {
+    const numberOfPeople = numberOfPeopleElement.value;
+    if (numberOfPeople == 0) {
+        document.querySelector('.data-input__header-error').style.display = "block";
+        numberOfPeopleElement.style.border = "2px solid #E17052"
+    }
+    else {
+        document.querySelector('.data-input__header-error').style.display = "none";
+        numberOfPeopleElement.style.border = "none"
+    }
+    localStorage.setItem('people',numberOfPeople);
+    calculateTip();
+}
+
+function calculateTip() {
+    const billAmount = localStorage.getItem('bill');
+    const tipNumber = localStorage.getItem('tip');
+    const numberOfPeople = localStorage.getItem('people');
+
+    const tipResult = (billAmount / numberOfPeople) * tipNumber;
+    const tipRound = Math.round(tipResult *100)/100;
+
+    const personTotal = (billAmount / numberOfPeople) + tipResult;
+    const personTotalRound = Math.round(personTotal *100)/100;
+
+    if (billAmount && tipNumber && numberOfPeople) {
+        tipPerPersonElement.innerText = '$' + tipRound;
+        totalPerPersonElement.innerText = '$' + personTotalRound;
+    }
+    else {
+        tipPerPersonElement.innerText = '$0.00';
+        totalPerPersonElement.innerText = '$0.00';
+    }
+}
+
+function clearLocalStorage () {
+    localStorage.clear();
+    tipPerPersonElement.innerText = '$0.00';
+    totalPerPersonElement.innerText = '$0.00';
+    inputBillElement.value = null;
+    numberOfPeopleElement.value = null;
+    tipPercentCustomElement.value = null;
+}
